2d62ed7b5c953825c3e905d6a49aa31248566461
Andrii <andriy30534>
2023-10-26 14:27:14 +0300

    fix styles

--- src/components/Form/InputForm.tsx
+++ src/components/Form/InputForm.tsx
@@ -1,8 +1,8 @@
+import { IFormInput } from '@/types';
 import { Box, FormHelperText, InputLabel, TextField, useTheme } from '@mui/material';
 import { FC, useEffect, useState } from 'react';
 import { Control, Controller, FieldError } from 'react-hook-form';
 import SvgSpriteIcon from '../Common/SvgSpriteIcon';
-import { IFormInput } from '@/types';
 import TooltipInfo from './TooltipInfo';
 
 interface InputFormProps {
@@ -19,15 +19,38 @@ interface InputFormProps {
 const InputForm: FC<InputFormProps> = ({ placeholder, control, name, alert, label, error, rows, isMulti = false }) => {
   const theme = useTheme();
   const [color, setColor] = useState(theme.palette.common.black);
-
+  const [isFocus, setIsFocus] = useState(false);
   useEffect(() => {
-    setColor(error ? theme.palette.error.main : theme.palette.common.black);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    if (isFocus) {
+      setColor(error ? theme.palette.error.main : theme.palette.primary.dark);
+    } else {
+      setColor(error ? theme.palette.error.main : theme.palette.common.black);
+    }
   }, [error]);
+
+  const onHover = () => {
+    if (!isFocus || error) {
+      setColor(theme.palette.common.black);
+    }
+    if (error) {
+      setColor(theme.palette.error.main);
+    }
+  };
+
+  const hover = () => {
+    if (!isFocus || error) {
+      setColor(theme.palette.primary.dark);
+    }
+    if (error) {
+      setColor(theme.palette.error.main);
+    }
+  };
 
   return (
     <Box sx={{ position: 'relative', color: { color } }}>
-      <InputLabel sx={{ marginBottom: '8px', color: 'inherit', fontSize: { xs: '16px', lg: '18px', fontWeight: '600' } }}>
+      <InputLabel
+        htmlFor={name}
+        sx={{ p: '0px', marginBottom: '8px', color: 'inherit', fontSize: { xs: '16px', lg: '18px', fontWeight: '600' } }}>
         {label}
       </InputLabel>
       <Controller
@@ -35,6 +58,7 @@ const InputForm: FC<InputFormProps> = ({ placeholder, control, name, alert, labe
         control={control}
         render={({ field }) => (
           <TextField
+            id={name}
             sx={{
               '& .MuiInputBase-input.MuiOutlinedInput-input': {
                 padding: '0 28px 0 0',
@@ -46,9 +70,14 @@ const InputForm: FC<InputFormProps> = ({ placeholder, control, name, alert, labe
             {...field}
             placeholder={placeholder}
             variant="outlined"
-            onMouseLeave={() => setColor(error ? theme.palette.error.main : theme.palette.common.black)}
-            onMouseEnter={() => setColor(error ? theme.palette.error.main : theme.palette.primary.dark)}
-            onFocus={() => setColor(error ? theme.palette.error.main : theme.palette.primary.dark)}
+            onMouseLeave={onHover}
+            onMouseEnter={hover}
+            onFocus={() => {
+              setColor(error ? theme.palette.error.main : theme.palette.primary.dark), setIsFocus(true);
+            }}
+            onBlur={() => {
+              setColor(error ? theme.palette.error.main : theme.palette.common.black), setIsFocus(false);
+            }}
             fullWidth
             multiline={isMulti}
             error={!!error}
@@ -63,14 +92,15 @@ const InputForm: FC<InputFormProps> = ({ placeholder, control, name, alert, labe
       {!!error && (
         <Box
           sx={{
+            position: 'absolute',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'flex-start',
             gap: 1,
             color: { color },
-            marginTop: '8px',
+            marginTop: '10px',
           }}>
-          <SvgSpriteIcon svgSpriteId="info_icon" />
+          <SvgSpriteIcon sx={{ fontSize: '20px' }} svgSpriteId="info_icon" />
           <FormHelperText sx={{ margin: '0px', color: { color }, fontSize: '14px' }}>{error.message}</FormHelperText>
         </Box>
       )}

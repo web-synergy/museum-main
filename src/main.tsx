cf304c5ebcfdf7b6d953ada8ad9065e2b70e6944
Anna Prutnik <prutnik.anna@gmail.com>
2023-10-27 14:31:10 +0300

    fix: chenge deployment settings to vercel, add proxy server

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/museum-main/'}>
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <App />

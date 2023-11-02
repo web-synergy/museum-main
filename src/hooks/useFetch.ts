fbe98dadeb004b9e135e9c282518eb5d5234b039
Andrii <andriy30534>
2023-11-02 09:15:52 +0200

    added isFulfiled status

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -4,6 +4,7 @@ interface LoadingDataResult<T> {
   data: T | null;
   isLoading: boolean;
   error: Error | null;
+  isFulfilled: boolean;
   eventLoading: (data: any) => void;
 }
 
@@ -11,12 +12,14 @@ export const useFetch = <T>(dataFetcher: (data: any) => Promise<{ data: T }>, ev
   const [data, setData] = useState<T | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
+  const [isFulfilled, setIsFulfilled] = useState<boolean>(false);
 
   const fetchData = async (requestData: any) => {
     setIsLoading(true);
     try {
       const response = await dataFetcher(requestData);
       setData(response.data);
+      setIsFulfilled(true);
       setIsLoading(false);
     } catch (error: any) {
       setError(error);
@@ -34,5 +37,5 @@ export const useFetch = <T>(dataFetcher: (data: any) => Promise<{ data: T }>, ev
     fetchData(data);
   };
 
-  return { data, isLoading, error, eventLoading };
+  return { data, isLoading, error, eventLoading, isFulfilled };
 };

5fa22b2e173339f1b3994c803146256ac1ce0060
Anna Prutnik <prutnik.anna@gmail.com>
2023-11-07 16:47:46 +0200

    fix: remove proxy for development

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,18 @@
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  console.log('env', env.VITE_SERVER_URL);
-  const config = {
-    plugins: [react()],
-    server: {
-      port: 8080,
-      host: true,
-      proxy: {
-        '/api': {
-          target: env.VITE_SERVER_URL,
-          changeOrigin: false,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
-      },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8080,
+    host: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '#root': resolve(__dirname),
     },
-    resolve: {
-      alias: {
-        '@': resolve(__dirname, 'src'),
-        '#root': resolve(__dirname),
-      },
-    },
-  };
-
-  return config;
+  },
 });

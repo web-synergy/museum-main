b4979a75459b96acf79f69beecc0514c1fe3200b
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-11-24 19:08:20 +0200

    feat: add styles for mobile screen

--- src/components/TeamCardModal/TeamCardModal.tsx
+++ src/components/TeamCardModal/TeamCardModal.tsx
@@ -1,5 +1,4 @@
-import { IconButton, Typography, Modal } from '@mui/material';
-
+import { IconButton, Typography, Modal, Box } from '@mui/material';
 import SvgSpriteIcon from '../Common/SvgSpriteIcon';
 import { ModalContainer } from './styles';
 
@@ -7,6 +6,48 @@ interface IModalTeamCard {
   handleClose: () => void;
   open: boolean;
 }
+
+// Об'єкт з командами та учасниками
+const teamsData = [
+  {
+    role: 'Business Analyst',
+    members: ['Maryna Yakovenko'],
+  },
+  {
+    role: 'Project Manager',
+    members: ['Anastasia Kuz'],
+  },
+  {
+    role: 'QA Mentor',
+    members: ['Yulia Klimuk'],
+  },
+  {
+    role: 'Design team',
+    members: ['Ilona Beneviat', 'Olga Lisniak', 'Mariia Popova'],
+  },
+  {
+    role: 'Front-end team',
+    members: ['Anna Prutnik', 'Andrii Bohonis', 'Artur Levchenko', 'Yevhen Onufrii', 'Stanislav Kuchma'],
+  },
+  {
+    role: 'Back-end team',
+    members: ['Evgeniy Malysh', 'Anatolii Omelchenko', 'Olha Ryzhkova', 'Andriy Sitarskiy', 'Vladyslav Bondyk', 'Dmytro Teliukov'],
+  },
+  {
+    role: 'QA team',
+    members: [
+      'Bohdan Stepanets',
+      'Pavlo Khlebnikov',
+      'Yuliia Hudyma',
+      'Alyona Slipak',
+      'Uliana Kravchuk',
+      'Lina Ragulina',
+      'Serhiy Shtefan-Antoniuk',
+    ],
+  },
+
+  // Добавьте другие роли и участников по мере необходимости
+];
 
 const TeamCardModal = ({ handleClose, open }: IModalTeamCard) => {
   return (
@@ -26,8 +67,38 @@ const TeamCardModal = ({ handleClose, open }: IModalTeamCard) => {
           }}>
           <SvgSpriteIcon svgSpriteId="burgerOpen_icon" />
         </IconButton>
-
-        <Typography sx={{ fontSize: { xs: '32px', md: '40px' } }}>Дякуємо!</Typography>
+        {/* Заголовок модального вікна */}
+        <Box sx={{ width: '100%', textAlign: 'center' }}>
+          <Typography variant="h5">Our great team</Typography>
+        </Box>
+        {/* Контент модального вікна з командами та учасниками */}
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '32px',
+            textAlign: 'center',
+            gap: '40px',
+          }}>
+          {teamsData.map((team, index) => (
+            <Box key={index}>
+              {/* Название роли */}
+              <Typography variant="h6" sx={{ color: 'orange', textAlign: 'center' }}>
+                {team.role}
+              </Typography>
+              {/* Список участников для каждой роли */}
+              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                {team.members.map((member, memIndex) => (
+                  <Typography key={memIndex} sx={{ marginTop: '8px', color: 'white', textAlign: 'center' }}>
+                    {member}
+                  </Typography>
+                ))}
+              </Box>
+            </Box>
+          ))}
+        </Box>
       </ModalContainer>
     </Modal>
   );
--- src/components/TeamCardModal/styles.ts
+++ src/components/TeamCardModal/styles.ts
@@ -5,14 +5,27 @@ export const ModalContainer = styled(Box)(({ theme }) => ({
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '734px',
+  width: '100%',
   padding: '56px',
   outline: 'none',
+  overflowY: 'auto',
+  maxHeight: '100vh',
   backgroundColor: theme.palette.common.black,
   color: theme.palette.common.white,
   boxShadow:
     '0px 2px 5px 0px rgba(0, 0, 0, 0.04), 0px 10px 10px 0px rgba(0, 0, 0, 0.03), 0px 22px 13px 0px rgba(0, 0, 0, 0.02), 0px 40px 16px 0px rgba(0, 0, 0, 0.01), 0px 62px 17px 0px rgba(0, 0, 0, 0.00)',
+
+  '&::-webkit-scrollbar': {
+    width: '6px',
+  },
+  '&::-webkit-scrollbar-track': {
+    backgroundColor: theme.palette.common.black,
+  },
+  '&::-webkit-scrollbar-thumb': {
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: '4px',
+  },
+
+  [theme.breakpoints.up('md')]: { width: '734px' },
+  [theme.breakpoints.up('lg')]: {},
 }));
